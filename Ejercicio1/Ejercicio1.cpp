73506559047207a0155f460d8ba612229285f641
NahuelRoman1990 <nahuel.90.roman@gmail.com>
2024-05-01 11:10:16 -0300

    eje1

--- Ejercicio1/Ejercicio1.cpp
+++ Ejercicio1/Ejercicio1.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <cstring>
-#include <cctype> // Para toupper
+#include <cctype>
 
 int main() {
     char animal1[20], animal2[20], animal3[20];
 
-    std::cout << "Introduce el nombre del primer animal: ";
-    std::cin >> animal1;
+    std::cout << "Introduce el nombre del primer animal: " << std::endl;
+    std::cin.getline(animal1, 20, '\n');
 
     int long1 = strlen(animal1);
-    char primerA = toupper(animal1[0]);
+    char primerA = std::toupper(animal1[0]);
 
-    std::cout << "Numero de letras para " << animal1 << " es " << long1 << ", letra: " << primerA << ".\n";
-    
-    std::cout << "Introduce el nombre del segundo animal: ";
-    std::cin >> animal2;
+    std::cout << "Numero de letras para " << animal1 << " es " << long1 << ", letra: " << primerA << ".\n" << std::endl;
+
+    std::cout << "Introduce el nombre del segundo animal: " << std::endl;
+    std::cin.getline(animal2, 20, '\n');
 
     int long2 = strlen(animal2);
-    char primerB = toupper(animal2[0]);
+    char primerB = std::toupper(animal2[0]);
 
-    std::cout << "Numero de letras para " << animal2 << " es " << long2 << ", letra: " << primerB << ".\n";
-    
-    std::cout << "Introduce el nombre del tercer animal: ";
-    std::cin >> animal3;
+    std::cout << "Numero de letras para " << animal2 << " es " << long2 << ", letra: " << primerB << ".\n" << std::endl;
+
+    std::cout << "Introduce el nombre del tercer animal: " << std::endl;
+    std::cin.getline(animal3, 20, '\n');
 
     int long3 = strlen(animal3);
-    char primerC = toupper(animal3[0]);
+    char primerC = std::toupper(animal3[0]);
 
-    std::cout << "Numero de letras para " << animal3 << " es " << long3 << ", letra: " << primerC << ".\n";
-    
+    std::cout << "Numero de letras para " << animal3 << " es " << long3 << ", letra: " << primerC << ".\n" << std::endl;
+
     return 0;
 }
+
 
